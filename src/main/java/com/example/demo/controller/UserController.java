0bdbc9376ca67b11ab4f6eba97c91999a9dc66bf
Aman Gurjar <amangurjar574@gmail.com>
2025-05-03 09:58:44 +0530

    RequestParam and PathVariable in query string  deep understanding

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,18 +4,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 @RestController
 public class UserController {
 
     @GetMapping("/name")
-   public  String getName(@RequestParam("name") String name)
+   public  String getName(@RequestParam(value = "id" , required = false) Integer id)
     {
-        return name;
-    }
-    @GetMapping("/id")
-   public  String getId(@RequestParam("id") int id)
-    {
-        return "Id : "+id;
+        HashMap<Integer,String> li=new HashMap<>();
+        li.put(1,"aaa");
+        li.put(2,"bbb");
+        li.put(3,"ccc");
+        li.put(4,"ddd");
+        li.put(5,"eee");
+        li.put(6,"fff");
+        li.put(7,"ggg");
+        if(id!=null){
+            String name=li.get(id);
+            if(name!=null)return name;
+            else return "not found..";
+        }
+        return li.values().toString();
     }
 
+
 }
+
