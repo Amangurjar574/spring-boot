9cd6b6323d456c11a1413c76d844eec9e5564ce2
Aman Gurjar <amangurjar574@gmail.com>
2025-05-09 00:18:29 +0530

    update code for exception hendling and restcontroller and add new point of REsponseEntity

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.entity.User;
 import com.example.demo.serviceimpl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -14,27 +16,27 @@ public class UserController {
     UserServiceImpl userService;
 
     @PostMapping("/create")
-    User createUser(@RequestBody User user)
+    public ResponseEntity<User> createUser(@RequestBody User user)
     {
-       return  userService.createUser(user);
+       return  new ResponseEntity<>(userService.createUser(user),HttpStatus.CREATED);
     }
     @DeleteMapping("/delete")
-    User deleteUser(@RequestParam("id") Integer id)
+    public ResponseEntity<User> deleteUser(@RequestParam("id") Integer id)
     {
-       return  userService.deleteUser(id);
+        return new ResponseEntity<>(userService.deleteUser(id),HttpStatus.OK);
     }
     @PutMapping("/update")
-    public User updateUser(@RequestBody User user)
+    public ResponseEntity<User> updateUser(@RequestBody User user)
     {
-        return userService.UpdateUSer(user);
+        return new ResponseEntity<>(userService.UpdateUSer(user),HttpStatus.OK);
     }
     @GetMapping("/user")
-    public List<User> getUser(@RequestParam("id") Integer id)
+    public ResponseEntity<List<User>> getUser(@RequestParam("id") Integer id)
     {
         if (id!=null)
-        return List.of(userService.getUser(id));
+        return new ResponseEntity<>(List.of(userService.getUser(id)), HttpStatus.OK);
         else
-            return userService.getAll();
+            return new ResponseEntity<>(userService.getAll(),HttpStatus.OK);
     }
 
 
--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,15 +1,28 @@
 package com.example.demo.exception;
 
+import com.example.demo.utility.ResponseStatus;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
 
 @RestControllerAdvice
-public class GlobalExceptionHandler extends RuntimeException{
-
+public class GlobalExceptionHandler{
 
     @ExceptionHandler(UserNotFoundException.class)
-    public String userNotFound(UserNotFoundException e)
+    public ResponseStatus userNotFound(UserNotFoundException e)
     {
-        return e.getMessage();
+       return new ResponseStatus(HttpStatus.NOT_FOUND.value(),e.getMessage());
+    }
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseStatus resourceNotFound(NoHandlerFoundException e)
+    {
+        return new ResponseStatus(HttpStatus.BAD_REQUEST.value(),e.getMessage());
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseStatus exception(Exception e)
+    {
+        return new ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR.value(),e.getMessage());
     }
 }
